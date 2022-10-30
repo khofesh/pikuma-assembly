d820a17ebc3b3e7466f6de823b56fedb2f4f0e99
fahmi ahmad <fahmiahmad@tutanota.com>
2022-10-30 17:42:46 +0700

    zeroed the ff

--- cleanmem/cleanmem.asm
+++ cleanmem/cleanmem.asm
@@ -15,6 +15,7 @@ Start:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     lda #0          ;A = 0
     ldx #$FF        ;X = #$FF
+    sta $FF         ;make sure $FF is zeroed before the loop starts
 MemLoop:
     dex             ;X--
     sta $0,X        ;store the value of A inside memory address $0 + X
